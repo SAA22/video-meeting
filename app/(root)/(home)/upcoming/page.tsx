a8b7038c681bda5a792163a1fb0a42d1bc3ac2b7
adrian andrei schneider <schneider.adrian.andrei@gmail.com>
2024-08-30 15:45:22 +0300

    first commit

---
+++ app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import CallList from '@/components/CallList';
+import React, { createContext } from 'react';
+
+const UpcomingPage = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Upcoming Meeting</h1>
+
+    <CallList type="upcoming"/>
+    </section>
+  );
+};
+
+export default UpcomingPage;
